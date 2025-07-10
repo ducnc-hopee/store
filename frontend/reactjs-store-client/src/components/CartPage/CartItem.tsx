9cd67bd96d2106d63808bc531b94ab39da60fd4e
Joelnwh <joelngwh@gmail.com>
2025-07-10 10:41:38 +0700

    fix styling and add button for cartItem

--- frontend/reactjs-store-client/src/components/CartPage/CartItem.tsx
+++ frontend/reactjs-store-client/src/components/CartPage/CartItem.tsx
@@ -4,9 +4,10 @@ import { Button } from "../ui/Button";
 
 type TCartItemProps = {
   data: TCartItem;
+  onRemove: (id: string) => void;
 };
 
-const CartItem: React.FC<TCartItemProps> = ({ data }) => {
+const CartItem: React.FC<TCartItemProps> = ({ data, onRemove }) => {
   const [quantity, setQuantity] = useState(Number(data.quantity));
   const [total, setTotal] = useState(Number(data.price) * Number(quantity));
 
@@ -20,7 +21,16 @@ const CartItem: React.FC<TCartItemProps> = ({ data }) => {
   return (
     <div className="flex py-[15px] border-b-2 border-[#E1E1E4] items-center">
       <div className="flex flex-row gap-x-[15px]">
-        <img className="w-[83px] h-[87px] object-cover rounded" src={data.image} alt={data.name} />
+        <div className="relative w-[83px] h-[87px]">
+          <img className="w-[83px] h-[87px] object-cover rounded" src={data.image} alt={data.name} />
+          <Button className="absolute top-[-18px] right-[-16.5px] bg-transparent border-none hover:bg-transparent shadow-none" onClick={() => onRemove(data.id)}>
+            <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 12 12" fill="none">
+              <path d="M6 12C9.31371 12 12 9.31371 12 6C12 2.68629 9.31371 0 6 0C2.68629 0 0 2.68629 0 6C0 9.31371 2.68629 12 6 12Z" fill="black" />
+              <path d="M7.8002 4.19995L4.2002 7.79995" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+              <path d="M4.2002 4.19995L7.8002 7.79995" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+            </svg>
+          </Button>
+        </div>
         <div className="flex flex-col justify-start text-left gap-y-[8px] w-[200px]">
           <h1 className="font-bold text-navy-blue whitespace-nowrap">{data.name}</h1>
           <p className="text-sm text-gray">Color: {data.color}</p>
@@ -31,11 +41,17 @@ const CartItem: React.FC<TCartItemProps> = ({ data }) => {
       <div className="flex justify-start font-medium text-navy-blue w-[43px] mr-[122px]">${data.price}</div>
 
       <div className="flex items-center w-[51px] h-[15px]">
-        <Button className="w-[12px] h-[15px] bg-[#E7E7EF] text-[#6B6C72] text-[10px] font-bold hover:bg-[#dcdce3] p-0 rounded-none" onClick={handleDecrement}>
+        <Button
+          className="w-[12px] h-[15px] bg-[#E7E7EF] text-[#6B6C72] text-[10px] font-bold hover:bg-[#dcdce3] p-0 rounded-none"
+          onClick={handleDecrement}
+        >
           -
         </Button>
         <h1 className="w-[26px] h-[15x] flex items-center justify-center bg-[#F0EFF2] text-[#BEBFC2] text-[10px]">{quantity}</h1>
-        <Button className="w-[12px] h-[15px] bg-[#E7E7EF] text-[#6B6C72] text-[10px] font-bold hover:bg-[#dcdce3] p-0 rounded-none" onClick={handleIncrement}>
+        <Button
+          className="w-[12px] h-[15px] bg-[#E7E7EF] text-[#6B6C72] text-[10px] font-bold hover:bg-[#dcdce3] p-0 rounded-none"
+          onClick={handleIncrement}
+        >
           +
         </Button>
       </div>
